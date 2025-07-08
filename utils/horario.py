dcfeecda2c374fb1fe8ac9bedd16d1b57842aa59
Juan Jose <cererbrotrading@gmail.com>
2025-07-08 09:51:30 -0500

    Update horario.py

--- utils/horario.py
+++ utils/horario.py
@@ -2,18 +2,20 @@
 
 from datetime import datetime
 import pytz
-from workalendar.america import Colombia
-from workalendar.usa import UnitedStates
+import holidays
 
 def obtener_hora_colombia():
-    zona_col = pytz.timezone("America/Bogota")
-    ahora = datetime.now(zona_col)
-    return ahora.strftime("%H:%M:%S")
+    tz = pytz.timezone("America/Bogota")
+    return datetime.now(tz)
 
 def es_dia_operativo():
-    hoy = datetime.now().date()
-    cal_col = Colombia()
-    cal_usa = UnitedStates()
-    es_laboral_col = cal_col.is_working_day(hoy)
-    es_laboral_usa = cal_usa.is_working_day(hoy)
-    return es_laboral_col and es_laboral_usa
+    tz = pytz.timezone("America/Bogota")
+    hoy = datetime.now(tz).date()
+
+    festivos_col = holidays.CountryHoliday('CO', years=hoy.year)
+    festivos_usa = holidays.CountryHoliday('US', years=hoy.year)
+
+    es_laborable = hoy.weekday() < 5  # Lunes a Viernes (0 a 4)
+    no_es_festivo = hoy not in festivos_col and hoy not in festivos_usa
+
+    return es_laborable and no_es_festivo
